f34ab2db2cff8983fd80655bbaa01e80b98764b6
Tarun Jangra <tarun@izap.in>
2016-07-10 02:31:52 +0530

    fix

--- _glyphs/compile.sh
+++ _glyphs/compile.sh
@@ -15,4 +15,4 @@ mkdir -p "${target}"
 fontcustom compile "${dir}" \
   "--output=${target}" \
   --font-name=icons --templates=css --no-hash --force --autowidth
-sed -i -E "s|\"./icons([^\"]+)\"|\"http://cf.jare.io/?u=http://tarunjangra.com/css/icons\1%3F$(date +%s)\"|g" "${target}/icons.css"
+sed -i -E "s|\"./icons([^\"]+)\"|\"http://tarunjangra.com/css/icons\1%3F$(date +%s)\"|g" "${target}/icons.css"
